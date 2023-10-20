039197ab736ef84c6540e725d1467be8f969192b
Collin Lang <collinlang0@gmail.com>
2023-10-19 21:11:37 -0400

    Darkens frog, adds fire visit, medium quality default

--- Assets/Scripts/UiManager.cs
+++ Assets/Scripts/UiManager.cs
@@ -330,8 +330,8 @@ public class UiManager : MonoBehaviour {
 				leftClickInstruction.localPosition = _leftClickStartPosition;
 				img.DOFade(1f, 1f);
 				leftClickInstruction.DOScale(Vector3.one * 0.6f, 0.7f).SetEase(Ease.InCubic).SetLoops(-1, LoopType.Yoyo);
-				leftClickInstruction.DOLocalMoveY(_leftClickStartPosition.y + (Screen.height * 0.04f), 0.7f).SetEase(Ease.InCubic).SetLoops(-1, LoopType.Yoyo);
-				leftClickInstruction.DOLocalMoveX(_leftClickStartPosition.x - (Screen.height * 0.01f), 0.7f).SetEase(Ease.InCubic).SetLoops(-1, LoopType.Yoyo);
+				leftClickInstruction.DOLocalMoveY(_leftClickStartPosition.y + 22f, 0.7f).SetEase(Ease.InCubic).SetLoops(-1, LoopType.Yoyo);
+				leftClickInstruction.DOLocalMoveX(_leftClickStartPosition.x - 6f, 0.7f).SetEase(Ease.InCubic).SetLoops(-1, LoopType.Yoyo);
 			} else {
 				img.DOFade(0f, 0.2f);
 			}
